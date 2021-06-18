4171c37ff3dcc33aab7d4564c18ea241dfeec7b8
Tyler Lawson <tylaw93@yahoo.com>
2021-06-17 23:33:40 -0500

    Stylizing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// array for the cities you search
 let cities = [];
 
 $("#findCity").on("click", function(event) {
@@ -6,7 +6,20 @@ $("#findCity").on("click", function(event) {
 
   let city = $("#cityInput").val(); 
 
+  $("#deleteCities").on("click", function() {
+    // empty the city names array
+    cities = [];
+    saveCities();
+    renderCities();
+  })
+
+
+getData(city)
+console.log(city)
+})
+
 function getCityImage(city){
+    // search unsplash for an image from the city
     const apiKey = "JGRm3Szc6q_F7l8YtT4vev4H0PyqEVI-NkaipHZ1Pmo"
     const photoSearch = `https://api.unsplash.com/search/photos?page=1&query=${city},&client_id=${apiKey}`
     fetch(photoSearch).then(function(response){
@@ -28,16 +41,16 @@ function displayCityImage(data){
         newArray.forEach(item =>{
             if(item.title === "building"){
                 num = entries
-                console.log(num)
+                //go through the entries and if one is tagged as a building save that image
             }
             
         });
     });
     let image = data.results[num].urls.full
-    console.log(num)
     document.getElementById("cityPhoto").src = image
 }
 function getData(city){
+    getCityImage(city)
     const apiKey = "c3d17af6b12c366aecbc2fdcb4467092"
     let weather5DaySearch = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&units=imperal&appid=${apiKey}`
     let weatherSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperal&appid=${apiKey}`
@@ -66,12 +79,6 @@ function getData(city){
         };
          
 
-
-
-getCityImage(city)
-
-getData(city)
-
 function displayData(data){
     
     let cityName = data.name;
@@ -89,13 +96,13 @@ function displayData(data){
   $("#cityWind").text(cityWind + ' MPH');
   $("#cityCondition").text(cityCondition);
 }
-})
+
 
 function displayFiveDayWeather(response) {
     // Display 5-day weather report
   
     // Clear deck before updating
-    $("#five-day-deck").empty();
+    $("#fiveDayCards").empty();
     for (let i = 6; i < 40; i += 8) {
       let cardDate = response.list[i].dt_txt;
       let date = new Date(cardDate).toLocaleDateString('en-US', {
@@ -106,13 +113,13 @@ function displayFiveDayWeather(response) {
       let cardTemp = Math.round(((response.list[i].main.temp) - 273.15) * 1.8) +32;;
       let cardHumid = Math.round(response.list[i].main.humidity);
       let iconSource = response.list[i].weather[0].icon;
-      let cardContainer = $("<section>").attr("class", "container")
-      let cardEl = $("<div>").attr("class", "card col-sm-12 col-lg-3");
+      let cardIcon = $("<img>").attr("src", `https://openweathermap.org/img/w/${iconSource}.png`);
+      let cardEl = $("<div>").attr("class", "card col-sm-12 col-lg-3 bg-light");
       let cardTableEl = $("<table>").attr("class", "cardBody table");
       let cardTitleEl = $("<h6>").attr("class", "card-title").text(date);
       let cardTrEl1 = $("<tr>")
       let cardTrEl2 = $("<tr>")
-      let cardIcon = $("<img>").attr("src", `https://openweathermap.org/img/w/${iconSource}.png`);
+      
       let cardTempEl = $("<td>").attr("class", "card-text").text(`${cardTemp} °F`);
       let cardTempText = $("<td>").attr("class", "card-text").text(`Temp: `);
       let cardHumidEl = $("<td>").attr("class", "card-text").text(`${cardHumid}%`);
@@ -132,8 +139,10 @@ $("#cityList").on("click", ".city", function(event) {
     event.preventDefault();
   
     let city = $(this).text();
-    getAPIs(city);
+    console.log(city)
+    getData(city)
   });
+
   function saveCities() {
     // Save city names.
     localStorage.setItem("cities", JSON.stringify(cities));
@@ -146,7 +155,7 @@ function renderCities() {
     // Render city names
     cities.forEach(city => {
   
-      let cityCard = $("<div>").attr("class", "card");
+      let cityCard = $("<div>").attr("class", "card bg-light");
       let cityCardBody = $("<div>").attr("class", "card-body city").text(city);
       cityCard.append(cityCardBody);
       $("#cityList").prepend(cityCard);
@@ -160,6 +169,7 @@ function renderCities() {
     // If high scores were retrieved from localStorage, update highScores array to it.
     if (storedCities !== null) {
       cities = storedCities;
+      renderCities()
     }
   }
   startup();
